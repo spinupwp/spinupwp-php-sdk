f22f2938c09e47889c77e53ffd6cc0dc9fa8a0b5
Ashley Rich <hello@ashleyrich.com>
2021-09-26 12:15:18 +0100

    Test exception handling

--- tests/Endpoints/SiteTest.php
+++ tests/Endpoints/SiteTest.php
@@ -1,6 +1,10 @@
 <?php
 
 use DeliciousBrains\SpinupWp\Endpoints\Site;
+use DeliciousBrains\SpinupWp\Exceptions\NotFoundException;
+use DeliciousBrains\SpinupWp\Exceptions\RateLimitException;
+use DeliciousBrains\SpinupWp\Exceptions\UnauthorizedException;
+use DeliciousBrains\SpinupWp\Exceptions\ValidationException;
 use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Response;
 use PHPUnit\Framework\TestCase;
@@ -61,5 +65,65 @@ class SiteTest extends TestCase
         );
 
         $this->assertEquals(100, $siteEndpoint->delete(1));
+    }
+
+    public function test_handling_validation_errors(): void
+    {
+        $client = Mockery::mock(Client::class);
+
+        $client->shouldReceive('request')->once()->with('POST', 'sites', [
+            'form_params' => [
+                'server_id' => 1,
+            ]
+        ])->andReturn(
+            new Response(422, [], '{"domain": ["The domain is required."]}')
+        );
+
+        try {
+            (new Site($client))->create(1, []);
+        } catch (ValidationException $e) {
+            //
+        }
+
+        $this->assertEquals(['domain' => ['The domain is required.']], $e->errors());
+    }
+
+    public function test_handling_404_errors(): void
+    {
+        $this->expectException(NotFoundException::class);
+
+        $client = Mockery::mock(Client::class);
+
+        $client->shouldReceive('request')->once()->with('GET', 'sites/1', [])->andReturn(
+            new Response(404)
+        );
+
+        (new Site($client))->get(1);
+    }
+
+    public function test_handling_401_errors(): void
+    {
+        $this->expectException(UnauthorizedException::class);
+
+        $client = Mockery::mock(Client::class);
+
+        $client->shouldReceive('request')->once()->with('GET', 'sites/1', [])->andReturn(
+            new Response(401)
+        );
+
+        (new Site($client))->get(1);
+    }
+
+    public function test_handling_429_errors(): void
+    {
+        $this->expectException(RateLimitException::class);
+
+        $client = Mockery::mock(Client::class);
+
+        $client->shouldReceive('request')->once()->with('GET', 'sites/1', [])->andReturn(
+            new Response(429)
+        );
+
+        (new Site($client))->get(1);
     }
 }
