b8b688f04c03507b78ccf8d74aec9b01df5bf6b6
Katelynn Barlowe <katelynn.barlowe@gmail.com>
2021-10-07 10:51:00 -0400

    remove trait

--- src/Resources/Site.php
+++ src/Resources/Site.php
@@ -2,13 +2,10 @@
 
 namespace DeliciousBrains\SpinupWp\Resources;
 
-use DeliciousBrains\SpinupWp\Traits\HasEvent;
 use DeliciousBrains\SpinupWp\Resources\Event as EventResource;
 
 class Site extends Resource
 {
-    use HasEvent;
-
     public function delete(): EventResource
     {
         return $this->endpoint->delete($this->id);
